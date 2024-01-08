c8dbe6b42019da0d049cbf9a459330d8def7cd90
Carlos Renier <carl.perezv@hotmail.com>
2024-01-08 15:54:45 -0300

    Mejora 2

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -3,70 +3,127 @@ import Button from "./Button";
 import Screen from "./Screen";
 
 const Calculadora = () => {
-  const [input, setInput] = useState("0");
-  // Calc la ocupare para guardar el primer elemento
-
-  // const ops = ["+", "-", "*", "/"];
+  // const [input, setInput] = useState("0");
+  const [calc, setCalc] = useState({
+    input: "",
+    value1: 0,
+    minus: "-",
+    mr: 0,
+    mc: 0,
+    operator1: "",
+  });
+  const [screen, setScreen] = useState("000000000000");
 
   const addNumbers = (nums) => {
-    let partialValue = "0";
+    // la cantidad maxima de la calculadora son 12 digitos
+    if (calc.input.length === 12) return;
 
-    const ops = ["+", "-", "*", "/"];
-    input === "00" || input === "0" ? setInput(nums) : setInput(input + nums);
+    if (calc.input.toString().includes(".") && nums.toString().includes("."))
+      return;
 
-    if (nums.toString().includes("-")) {
-      // operator1 = partialValue;
-      partialValue = input + nums;
-
-      console.log("oli");
+    if (calc.value1) {
+      setCalc({ ...calc, input: nums + calc.input });
     }
 
-    // opcion que omita el digito en la pantalla pero agregue en la equacion
+    calc.input === 0 || calc.input === "0" || calc.input === "00"
+      ? setCalc({ ...calc, input: nums })
+      : setCalc({ ...calc, input: calc.input + nums });
 
-    console.log("ASD");
+    if (calc.input.toString().charAt(0) === ".") {
+      setCalc({ ...calc, input: "0." + nums });
+    }
+    console.log(
+      "value 1",
+      calc.value1,
+      "operador ",
+      calc.operator1,
+      "input ",
+      calc.input,
+    );
   };
 
-  const minusPlus = () => {
-    (input === "0" || input === "00"
-      ? setInput(input)
-      : input.charAt(0) === "-"
-        ? setInput(input.substring(1))
-        : setInput("-" + input)) ||
-      (setInput === "0" || setInput === "00"
-        ? setInput(input)
-        : input.charAt(0) === "-"
-          ? setInput(input.substring(1))
-          : setInput("-" + input));
+  // cambia el valor a negativo
+  const minusPlusButton = () => {
+    // if (calc.minus) {
+    //   console.log("Tiene minus", calc.input);
+    // }
+
+    console.log("Tiene minus", calc.minus);
+    // calc.input.charAt(0) === "-"
+    //   ? setCalc(calc.input.substring(1))
+    //   : setCalc("-" + calc.input);
+    // console.log("el minus " + calc.input);
+    // console.log("el minus ", "-" + calc.input);
   };
 
   const deleteNumber = () => {
-    input.length === 1 ? setInput("0") : setInput(input.slice(0, -1));
+    calc.input.length === 1
+      ? setCalc({ ...calc, input: "" })
+      : setCalc({ ...calc, input: calc.input.slice(0, -1) });
   };
 
-  const operators = (value) => {
-    // let val1 = null;
-    // const op = value;
-    // console.log(value);
-    // if (val1 === null) {
-    //   val1 = input;
+  const operatorButton = (value) => {
+    // aca la logica es que si value1 ya tiene un dato se calcula
+    if (calc.value1) {
+      setScreen(calc.input);
+      console.log("input del calc ", calc.input);
 
-    //   console.log(val1);
-    // }
+      try {
+        const newCalc = {
+          ...calc,
+          value1: eval(calc.value1 + calc.operator1 + calc.input).toString(),
+          operator1: value,
+          input: "",
+        };
 
-    const operator1 = input + value;
-    console.log("value " + value);
-    console.log("input " + input);
-    console.log("operator " + operator1 + value);
-
-    setInput("0");
+        setCalc(newCalc);
+        setScreen(newCalc.value1);
+      } catch (e) {
+        // console.log(e);
+        setScreen(calc.value1);
+        setCalc({
+          ...calc,
+          operator1: value,
+        });
+      }
+    }
+    // Si value1 no tiene datos
+    if (calc.value1 === 0) {
+      setScreen(calc.input);
+      setCalc({
+        ...calc,
+        value1: calc.input,
+        operator1: value,
+        input: "",
+      });
+    }
+    console.log("fuera ", calc.operator1);
   };
 
   const calcResult = () => {
-    setInput(eval(input));
+    const newCalc = {
+      ...calc,
+      value1: eval(calc.value1 + calc.operator1 + calc.input).toString(),
+      input: "",
+    };
+
+    setCalc(newCalc);
+    setScreen(newCalc.value1);
+  };
+
+  const clearButton = () => {
+    setScreen("000000000000");
+
+    setCalc({
+      input: "",
+      value1: 0,
+      minus: 0,
+      operator: "",
+    });
   };
 
   return (
-    <body>
+    <>
       {/* container bg-zinc-800 mx-auto rounded-3xl mt-10 px-8 p-4 shadow-xl w-6/12 */}
       <main className='container mx-auto mt-24 block h-[610px] w-[520px] rounded-3xl border-x-4 border-b-2 border-s-4 border-t-8 border-gray-600 bg-gradient-to-r from-stone-800 via-stone-950 to-black p-2 text-white shadow-lg shadow-purple-800'>
         <header className='p-3'>
@@ -86,7 +143,7 @@ const Calculadora = () => {
               </div>
             </div>
           </div>
-          <Screen input={input}></Screen>
+          <Screen value={calc.input} placeholder={screen}></Screen>
           {/* <input
             className="w-full border-8 border-b-black border-y-8 border-gray-500 mt-2 text-right text-gray-700 text-7xl block  rounded-xl bg-emerald-200"
             type="text"
@@ -103,32 +160,70 @@ const Calculadora = () => {
             <Button symbol={"MC"}>MC</Button>
             <Button symbol={"M-"}>M-</Button>
             <Button symbol={"M+"}>M+</Button>
-            <Button symbol={"/"}>÷</Button>
-            <Button symbol={"minus"}>+/-</Button>
-            <Button symbol={"7"} onClick={addNumbers}>
+            <Button symbol={"/"} handleClick={operatorButton}>
+              ÷
+            </Button>
+            <Button symbol={"minus"} handleClick={minusPlusButton}>
+              +/-
+            </Button>
+            <Button symbol={"7"} handleClick={addNumbers}>
               7
             </Button>
-            <Button symbol={"8"}>8</Button>
-            <Button symbol={"9"}>9</Button>
-            <Button symbol={"*"}>X</Button>
-            <Button symbol={"DEL"}>►</Button>
-            <Button symbol={"4"}>4</Button>
-            <Button symbol={"5"}>5</Button>
-            <Button symbol={"6"}>6</Button>
-            <Button symbol={"-"}>-</Button>
-            <Button symbol={"C"}>C/AC ON</Button>
-            <Button symbol={"1"}>1</Button>
-            <Button symbol={"2"}>2</Button>
-            <Button symbol={"3"}>3</Button>
-            <Button symbol={"+"}>+</Button>
-            <Button symbol={"0"}>0</Button>
-            <Button symbol={"00"}>00</Button>
-            <Button symbol={"."}>.</Button>
-            <Button symbol={"="}>=</Button>
+            <Button symbol={"8"} handleClick={addNumbers}>
+              8
+            </Button>
+            <Button symbol={"9"} handleClick={addNumbers}>
+              9
+            </Button>
+            <Button symbol={"*"} handleClick={operatorButton}>
+              X
+            </Button>
+            <Button symbol={"DEL"} handleClick={deleteNumber}>
+              ►
+            </Button>
+            <Button symbol={"4"} handleClick={addNumbers}>
+              4
+            </Button>
+            <Button symbol={"5"} handleClick={addNumbers}>
+              5
+            </Button>
+            <Button symbol={"6"} handleClick={addNumbers}>
+              6
+            </Button>
+            <Button symbol={"-"} handleClick={operatorButton}>
+              -
+            </Button>
+            <Button symbol={"C"} handleClick={clearButton}>
+              C/AC ON
+            </Button>
+            <Button symbol={"1"} handleClick={addNumbers}>
+              1
+            </Button>
+            <Button symbol={"2"} handleClick={addNumbers}>
+              2
+            </Button>
+            <Button symbol={"3"} handleClick={addNumbers}>
+              3
+            </Button>
+            <Button symbol={"+"} handleClick={operatorButton}>
+              +
+            </Button>
+            <Button symbol={"0"} handleClick={addNumbers}>
+              0
+            </Button>
+            <Button symbol={"00"} handleClick={addNumbers}>
+              00
+            </Button>
+            <Button symbol={"."} handleClick={addNumbers}>
+              .
+            </Button>
+            <Button symbol={"="} handleClick={calcResult}>
+              =
+            </Button>
           </div>
         </section>
       </main>
-    </body>
+    </>
   );
 };
 
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,12 +1,13 @@
-const Screen = ({ input }) => {
+const Screen = ({ value, placeholder }) => {
   return (
     <input
-      type='number'
+      type='text'
       className='block h-24 w-full rounded-xl border-x-8 border-b-8 border-t-2 border-zinc-950 bg-gradient-to-r from-green-300 to-emerald-200 p-2 px-2 text-right font-digit text-7xl text-gray-700'
       disabled
-    >
-      {input}
-    </input>
+      max={12}
+      placeholder={placeholder}
+      value={value}
+    />
   );
 };
 
